77fab93fc8fa8559563cce69992fe0c6a2fd33ad
Tanner <hunsucker@wisc.edu>
2016-02-14 20:42:39 -0600

    all functionality working

--- app/src/main/java/com/example/tanner/quizapp/MainActivity.java
+++ app/src/main/java/com/example/tanner/quizapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.tanner.quizapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            return;
+        }
     }
+
+    public void playPressed(View view) {
+
+        Intent play = new Intent(this, PlayActivity.class);
+        startActivity(play);
+
+    }
+
+
 }
